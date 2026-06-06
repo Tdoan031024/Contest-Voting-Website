8e2000dd0eccf1d3b4eadc1594cb31455b400a64
Tdoan030124 <tuyendoan369@gmail.com>
2026-06-06 20:51:48 +0700

    chore: optimize next build memory to prevent oom on deploy

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   transpilePackages: ['@huitfest/shared'],
   basePath: '/admin',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
